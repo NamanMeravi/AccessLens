5e25b65360f0addb590a2e81e135f77b4e0642ac
Anish Dhomase <anishdhomase@gmail.com>
2025-08-20 12:13:17 +0530

    feat(frontend): Add custom cursor

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,10 +3,12 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import ToastContext from "./contexts/ToastContext.jsx";
+import FollowCursor from "./components/Animation/FollowCursor.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <ToastContext>
+      <FollowCursor color="#2f4b776d" />
       <App />
     </ToastContext>
   </StrictMode>
